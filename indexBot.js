c9959cdec9465b7407eddd195b7ec0026394deba
LoggeL <hyper.xjo@gmx.de>
2021-07-23 02:45:52 +0200

    split bot from server, add pagination

---
+++ indexBot.js
@@ -0,0 +1,80 @@
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
+
+const prefix = 'https://cdn.discordapp.com/attachments/838682121975234571/'
+
+const config = require('./config.json')
+const client = new Discord.Client()
+
+client.on('ready', async () => {
+    console.log('Logged in as ' + client.user.tag)
+
+    if (!await db.schema.hasTable('data')) {
+        await db.schema.createTable('data', (table) => {
+            table.increments('id').primary()
+            table.string('imgKey')
+            table.string('vidKey')
+            table.string('name')
+            table.timestamp('createdAt')
+        })
+    }
+})
+
+let tempStorage = {};
+client.on('messageUpdate', (oldMessage, newMessage) => {
+    if (!newMessage.author.bot) return;
+    if (newMessage.author.id != config.user) return;
+    if (oldMessage.content === newMessage.content) return;
+
+    // Collect Videos
+    if (newMessage.content.includes(' (100%) ')) {
+        const videoURL = newMessage.content.split('\n').filter(line => line.includes('.mp4')).pop();
+        if (!videoURL) return;
+        const fileName = videoURL.split('/').pop()
+        const fileEnding = fileName.split('.').pop()
+        const fileWithoutEnding = fileName.replace('.' + fileEnding, '')
+        if (!fileWithoutEnding || !tempStorage[fileWithoutEnding]) return
+        const vidKey = videoURL.replace(prefix, '').split('/')[0];
+        tempStorage[fileWithoutEnding].vidKey = vidKey;
+        tempStorage[fileWithoutEnding].name = fileWithoutEnding;
+        tempStorage[fileWithoutEnding].createdAt = Date.now()
+        console.log('Vid', fileWithoutEnding, vidKey)
+        setTimeout(async () => {
+            const duplicate = await db('data').where('vidKey', vidKey).select('*')
+
+            const imgResponse = await fetch(`${prefix}${tempStorage[fileWithoutEnding].imgKey}/${tempStorage[fileWithoutEnding].name}.jpg`, { method: 'HEAD' })
+            if (imgResponse.status == 403) return delete tempStorage[fileWithoutEnding];
+
+            const vidResponse = await fetch(`${prefix}${tempStorage[fileWithoutEnding].vidKey}/${tempStorage[fileWithoutEnding].name}.mp4`, { method: 'HEAD' })
+            if (vidResponse.status == 403) return delete tempStorage[fileWithoutEnding];
+
+            if (duplicate.length == 0) await db('data').insert(tempStorage[fileWithoutEnding])
+            delete tempStorage[fileWithoutEnding];
+
+            // Garbage collection
+            for (key in tempStorage) {
+                if (!tempStorage[key].createdAt || Date.now() - tempStorage[key].createdAt > 3600000) {
+                    delete tempStorage[key]
+                    console.log('garbage', key)
+                }
+            }
+            // Give it 10 seconds to post the final picture
+        }, 10000)
+
+        // Collect Images
+    } else if (newMessage.content.includes('.jpg')) {
+        const fileUrl = newMessage.content.split('\n').filter(line => line.includes('.jpg')).pop();
+        if (!fileUrl) return;
+        const fileName = fileUrl.split('/').pop();
+        const fileEnding = fileName.split('.').pop()
+        const fileWithoutEnding = fileName.replace('.' + fileEnding, '')
+        if (!fileWithoutEnding) return
+        const imgKey = fileUrl.replace(prefix, '').split('/')[0];
+        if (!tempStorage[fileWithoutEnding]) tempStorage[fileWithoutEnding] = {}
+        tempStorage[fileWithoutEnding].imgKey = imgKey;
+        tempStorage[fileWithoutEnding].createdAt = Date.now()
+        // console.log('Img', fileWithoutEnding, imgKey)
+    }
+})
+
+client.login(config.token);
--- webview/main.js
+++ webview/main.js
@@ -1,121 +1,166 @@
 const prefix = 'https://cdn.discordapp.com/attachments/838682121975234571'
+let currentPage = 1
+let initialLoad = true
+let maxPages
 
-fetch('../data').then(response => response.json().then(pictures => {
-    pictures = pictures.reverse()
+const pagination = document.querySelector('.pagination')
+const pageInput = pagination.querySelector('input')
 
-
-
-    const ul = document.createElement('ul')
-    const loadCount = 200 //pictures.length
-    let count = 0
-
-    const progress = document.getElementById('progress-in')
-    const percentage = document.getElementById('percentage')
-    function doCount() {
-        count++
-        percentage.innerHTML = Math.round(count / loadCount * 100) + '% (' + count + '/' + loadCount + ')'
-        progress.style.width = Math.round(count / loadCount * 100) + '%'
-        if (count >= loadCount) {
-            setTimeout(() => {
-                const preloader = document.getElementById('preloader')
-                preloader.style.opacity = 0
-                setTimeout(() => preloader.remove(), 1000)
-            }, 1000)
-        }
-    }
-
-    for (let i = 0; i < pictures.length; i++) {
-
-        if (i >= loadCount) break
-
-        const picture = pictures[i]
-        const li = document.createElement('li')
-
-        const a = document.createElement('a')
-        const video = document.createElement('video')
-        const preloadImg = document.createElement('img')
-        preloadImg.className = 'preload'
-        preloadImg.style.opacity = '1'
-        const shadow = document.createElement('div')
-        shadow.className = 'shadow'
-        const text = document.createElement('div')
-        text.className = 'imageInfo'
-        text.innerText = picture.name.split('_').splice(1).join(' ')
-        li.onclick = () => {
-            const vid = li.querySelector('video')
-            const playBtn = li.querySelector('.playButtonWrapper')
-            if (playBtn) playBtn.remove()
-            vid.style.opacity = 1
-            setTimeout(() => {
-                vid.play()
-            }, 1000)
-        }
-
-        const newTab = document.createElement('div')
-        newTab.className = 'newTab'
-
-        const newTabImageLink = document.createElement('a')
-        newTabImageLink.href = `${prefix}/${picture.imgKey}/${picture.name}.jpg`
-        newTabImageLink.target = '_blank'
-
-        const newTabImage = document.createElement('img')
-        newTabImage.src = 'show-img.svg'
-        newTabImage.className = 'newTabImage'
-        newTabImage.title = 'Open Image in new tab'
-        newTabImage.alt = 'Open Image in new tab'
-
-        const newTabVideoLink = document.createElement('a')
-        newTabVideoLink.href = `${prefix}/${picture.vidKey}/${picture.name}.mp4`
-        newTabVideoLink.target = '_blank'
-
-        const newTabVideo = document.createElement('img')
-        newTabVideo.src = 'show-vid.svg'
-        newTabVideo.className = 'newTabImage'
-        newTabVideo.title = 'Download Video'
-        newTabVideo.alt = 'Download Video'
-
-        newTabVideoLink.append(newTabVideo)
-        newTab.append(newTabVideoLink)
-
-        newTabImageLink.append(newTabImage)
-        newTab.append(newTabImageLink)
-
-        const playButtonWrapper = document.createElement('div')
-        playButtonWrapper.className = 'playButtonWrapper'
-
-        const playButton = document.createElement('img')
-        playButton.src = 'play.svg'
-
-        playButtonWrapper.append(playButton)
-        a.append(playButtonWrapper)
-
-        const newTabShadow = document.createElement('div')
-        newTabShadow.className = 'newTabShadow'
-        a.append(newTabShadow)
-
-        video.setAttribute('src', `${prefix}/${picture.vidKey}/${picture.name}.mp4`)
-        video.setAttribute('preload', 'none')
-        video.setAttribute('poster', `${prefix}/${picture.imgKey}/${picture.name}.jpg`)
-
-        preloadImg.onload = () => {
-            preloadImg.style['background-image'] = `url('${prefix}/${picture.imgKey}/${picture.name}.jpg')`
-            doCount()
-        }
-        preloadImg.onerror = () => {
-            doCount()
-            preloadImg.parentElement.parentElement.remove()
-            return
-        }
-        preloadImg.src = `${prefix}/${picture.imgKey}/${picture.name}.jpg`
-
-        a.append(newTab)
-        a.append(video)
-        a.append(preloadImg)
-        a.append(text)
-        a.append(shadow)
-        li.append(a)
-        ul.append(li)
-    }
-    document.getElementById('gallery').append(ul)
+// Loads pagination
+fetch('../pageCount').then(response => response.json().then(pages => {
+    maxPages = pages
+    pagination.querySelector('span').innerText = maxPages
+    pageInput.setAttribute('min', 1)
+    pageInput.setAttribute('max', maxPages)
+    pageInput.value = currentPage
 }))
 
+pageInput.onchange = e => {
+    if (e.target.value > maxPages) e.target.value = maxPages
+    if (e.target.value < 1) e.target.value = 1
+    changePage(e.target.value)
+}
+
+pagination.firstElementChild.onclick = e => {
+    if (currentPage == 1) return
+    changePage(currentPage - 1)
+}
+
+pagination.lastElementChild.onclick = e => {
+    if (currentPage == maxPages) return
+    changePage(currentPage + 1)
+}
+
+function changePage(page) {
+    pagination.lastElementChild.className = ''
+    pagination.firstElementChild.className = ''
+    console.log(page)
+    currentPage = page
+    pageInput.value = currentPage
+    if (page == 1) pagination.firstElementChild.className = 'disabled'
+    else if (page == maxPages) pagination.lastElementChild.className = 'disabled'
+    popuplateGallery()
+}
+
+popuplateGallery()
+
+function popuplateGallery() {
+    fetch('../data?page=' + currentPage).then(response => response.json().then(pictures => {
+        document.getElementById('gallery').innerHTML = ''
+
+        const ul = document.createElement('ul')
+        const loadCount = pictures.length
+        let count = 0
+
+        const progress = document.getElementById('progress-in')
+        const percentage = document.getElementById('percentage')
+        function doCount() {
+            count++
+            percentage.innerHTML = Math.round(count / loadCount * 100) + '% (' + count + '/' + loadCount + ')'
+            progress.style.width = Math.round(count / loadCount * 100) + '%'
+            if (count >= loadCount) {
+                setTimeout(() => {
+                    const preloader = document.getElementById('preloader')
+                    preloader.style.opacity = 0
+                    setTimeout(() => preloader.remove(), 1000)
+                    initialLoad = false
+                }, 1000)
+            }
+        }
+
+        for (let i = 0; i < pictures.length; i++) {
+
+            if (i >= loadCount) break
+
+            const picture = pictures[i]
+            const li = document.createElement('li')
+
+            const a = document.createElement('a')
+            const video = document.createElement('video')
+            const preloadImg = document.createElement('img')
+            preloadImg.className = 'preload'
+            preloadImg.style.opacity = '1'
+            const shadow = document.createElement('div')
+            shadow.className = 'shadow'
+            const text = document.createElement('div')
+            text.className = 'imageInfo'
+            text.innerText = picture.name.split('_').splice(1).join(' ')
+            li.onclick = () => {
+                const vid = li.querySelector('video')
+                const playBtn = li.querySelector('.playButtonWrapper')
+                if (playBtn) playBtn.remove()
+                vid.style.opacity = 1
+                setTimeout(() => {
+                    vid.play()
+                }, 1000)
+            }
+
+            const newTab = document.createElement('div')
+            newTab.className = 'newTab'
+
+            const newTabImageLink = document.createElement('a')
+            newTabImageLink.href = `${prefix}/${picture.imgKey}/${picture.name}.jpg`
+            newTabImageLink.target = '_blank'
+
+            const newTabImage = document.createElement('img')
+            newTabImage.src = 'show-img.svg'
+            newTabImage.className = 'newTabImage'
+            newTabImage.title = 'Open Image in new tab'
+            newTabImage.alt = 'Open Image in new tab'
+
+            const newTabVideoLink = document.createElement('a')
+            newTabVideoLink.href = `${prefix}/${picture.vidKey}/${picture.name}.mp4`
+            newTabVideoLink.target = '_blank'
+
+            const newTabVideo = document.createElement('img')
+            newTabVideo.src = 'show-vid.svg'
+            newTabVideo.className = 'newTabImage'
+            newTabVideo.title = 'Download Video'
+            newTabVideo.alt = 'Download Video'
+
+            newTabVideoLink.append(newTabVideo)
+            newTab.append(newTabVideoLink)
+
+            newTabImageLink.append(newTabImage)
+            newTab.append(newTabImageLink)
+
+            const playButtonWrapper = document.createElement('div')
+            playButtonWrapper.className = 'playButtonWrapper'
+
+            const playButton = document.createElement('img')
+            playButton.src = 'play.svg'
+
+            playButtonWrapper.append(playButton)
+            a.append(playButtonWrapper)
+
+            const newTabShadow = document.createElement('div')
+            newTabShadow.className = 'newTabShadow'
+            a.append(newTabShadow)
+
+            video.setAttribute('src', `${prefix}/${picture.vidKey}/${picture.name}.mp4`)
+            video.setAttribute('preload', 'none')
+            video.setAttribute('poster', `${prefix}/${picture.imgKey}/${picture.name}.jpg`)
+
+            preloadImg.onload = () => {
+                preloadImg.style['background-image'] = `url('${prefix}/${picture.imgKey}/${picture.name}.jpg')`
+                if (initialLoad) doCount()
+            }
+
+            preloadImg.onerror = () => {
+                if (initialLoad) doCount()
+                preloadImg.parentElement.parentElement.remove()
+                return
+            }
+            preloadImg.src = `${prefix}/${picture.imgKey}/${picture.name}.jpg`
+
+            a.append(newTab)
+            a.append(video)
+            a.append(preloadImg)
+            a.append(text)
+            a.append(shadow)
+            li.append(a)
+            ul.append(li)
+        }
+        document.getElementById('gallery').append(ul)
+    }))
+}
